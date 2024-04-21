cc5b29be7aa0394ce8721b4bc58a5e4eb400b299
Xi Huang <sam.xihuang@gmail.com>
2024-04-21 15:08:48 +0200

    Add debug message

--- traj_transformer/utils/util_mp.py
+++ traj_transformer/utils/util_mp.py
@@ -93,9 +93,11 @@ class MP4Transformer:
             self.traj_gen.set_initial_conditions(init_time, condition_pos, condition_vel)
             self.traj_gen.set_duration(self.duration, self.dt, include_init_time=True)
         except Exception as e:
-            print(self.traj_gen.basis_gn.pre_compute_length_factor)
-            print(init_time)
-            print(init_time.max())
+            print("self.traj_gen.basis_gn.pre_compute_length_factor", self.traj_gen.basis_gn.pre_compute_length_factor)
+            print("init_time.max()", init_time.max())
+            print("init_time.max() > self.traj_gen.basis_gn.pre_compute_length_factor",
+                  init_time.max() > self.traj_gen.basis_gn.pre_compute_length_factor)
+            raise e
 
         result_dict = self.traj_gen.get_trajs()
         return result_dict
